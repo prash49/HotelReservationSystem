c572aff724e0803257d0a85c86b395aa5fb3c3d5
Prashanth <imprash4u@gmail.com>
2021-09-22 08:22:50 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystemMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hotelreservationsystem;
+
+/**
+ * Hello world!
+ */
+public class HotelReservationSystemMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
